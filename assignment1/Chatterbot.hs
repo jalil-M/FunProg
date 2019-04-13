6d4da69c49af561d10da36dd2f27e4bdc754db20
Abdeljalil Moussa <jalil.msa@gmail.com>
2019-04-13 19:18:35 +0200

    without indentation

--- assignment1/Chatterbot.hs
+++ assignment1/Chatterbot.hs
@@ -27,23 +27,19 @@ type BotBrain = [(Phrase, [Phrase])]
 
 stateOfMind :: BotBrain -> IO (Phrase -> Phrase)
 {- TO BE WRITTEN -}
--- stateOfMind _ = return id
-stateOfMind brain = 
-	do
-		rand <- randomIO :: IO Float
-		return (rulesApply((map . map2)(id, pick rand) brain))
+stateOfMind mind =
+  do
+    rand <- randomIO :: IO Float
+    return (rulesApply((map . map2)(id, pick rand) brain))
 
 rulesApply :: [PhrasePair] -> Phrase -> Phrase
 {- TO BE WRITTEN -}
--- rulesApply _ = id
 rulesApply = try.transformationsApply "*" reflect
 
 reflect :: Phrase -> Phrase
 {- TO BE WRITTEN -}
--- reflect = id
 reflect = map reflectWord
-	where reflectWord = try (flip lookup reflections)
-
+  where reflectWord = try (flip lookup reflections)
 
 reflections =
   [ ("am",     "are"),
@@ -64,6 +60,7 @@ reflections =
     ("you",    "me")
   ]
 
+
 ---------------------------------------------------------------------------------
 
 endOfDialog :: String -> Bool
@@ -75,12 +72,13 @@ present = unwords
 prepare :: String -> Phrase
 prepare = reduce . words . map toLower . filter (not . flip elem ".,:;*!#%&|") 
 
-{- To Do: jonathanloganmoran -}
 rulesCompile :: [(String, [String])] -> BotBrain
-{- TO BE WRITTEN: Modified 9/4 by jalil-M -}
-rulesCompile = (map . map2) ((words . map toLower), map words)
+{- TO BE WRITTEN -}
+rulesCompile = (map .map2) ((words . map toLower), map words)
+
 
 --------------------------------------
+
 
 reductions :: [PhrasePair]
 reductions = (map.map2) (words, words)
@@ -100,11 +98,9 @@ reductions = (map.map2) (words, words)
 reduce :: Phrase -> Phrase
 reduce = reductionsApply reductions
 
-
-{- To-do 9/4: @jonathanloganmoran -}
 reductionsApply :: [PhrasePair] -> Phrase -> Phrase
 {- TO BE WRITTEN -}
---reductionsApply _ = id
+reductionsApply = fix . try . transformationsApply "*" id
 
 
 -------------------------------------------------------
@@ -113,63 +109,43 @@ reductionsApply :: [PhrasePair] -> Phrase -> Phrase
 
 -- Replaces a wildcard in a list with the list given as the third argument
 substitute :: Eq a => a -> [a] -> [a] -> [a]
-{- TO BE WRITTEN: -}
---  MODIFIED: 9/4 by jonathanloganmoran
-substitute _ [] _ = []
--- Cases to handle: 
----- Exists for x <-> s where both are present 
+substitute _ _ _ = []
+{- TO BE WRITTEN -}
 substitute f (x:xs) s
----- Does not exist for f: append to position and form returned list
-    | f == x = s ++ substitute f xs s
----- Does not exist for x: tail recursion (cons)
-    | otherwise = f : substitute f xs s
+  | f == x = s ++ substitute f xs s
+  | otherwise = f : substitute f xs s
+
 
 -- Tries to match two lists. If they match, the result consists of the sublist
 -- bound to the wildcard in the pattern list.
 match :: Eq a => a -> [a] -> [a] -> Maybe [a]
 match _ _ _ = Nothing
 {- TO BE WRITTEN -}
---  MODIFIED: 9/4 by jonathanloganmoran
-match _ _ _ = Nothing
--- Cases to handle: 
---- Default case: wildcard matches
 match _ [] [] = Just []
---- Second case: input is empty
 match _ _ [] = Nothing
---- Third case: wildcard does not exist in sublist
 match _ [] _ = Nothing
 
 match wc (x:ps) (s:sl)
-    | x == s = match wc ps sl
-    | wc /= x = Nothing
-    | otherwise longerWildcardMatch (x:ps) (s:sl) `orElse` singleWildcardMatch (x:ps) (s:sl)
+  | x == s = match wc ps sl
+  | wc /= x = Nothing
+  | otherwise longerWildcardMatch (x:ps) (s:sl) `orElse` singleWildcardMatch (x:ps) (s:sl)
 
-{- MODIFIED 9/4: @jonathanloganmoran -}
+
+
 -- Helper function to match
 singleWildcardMatch, longerWildcardMatch :: Eq a => [a] -> [a] -> Maybe [a]
--- Cases to handle:
---- Expected case: first matches second
-singleWildcardMatch [] [] = Just []
---- Second case: wildcard matches list
-singleWildcardMatch _ [] = Nothing
---- Third case: list matches wildcard
-singleWildcardMatch [] _ = Nothing
 singleWildcardMatch (wc:ps) (x:xs) = mmap (const [x]) wc ps xs
+singleWildcardMatch [] [] = Just []
+singleWildcardMatch _ [] = Nothing
+singleWildcardMatch [] _ Nothing
 
-<<<<<<< HEAD
-{- TO BE WRITTEN -}
--- Cases to handle:
---- Expected case: wildcard retained, matches remaining list
-longerWildcardMatch [] [] = Just []
---- Second case: wildcard removed, sublist matches after
-longerWildcardMatch _ [] = Nothing
---- Third case: wildcard removed, sublist matches before
-longerWildcardMatch [] _ = Nothing
 longerWildcardMatch (wc:ps) (x:xs) = mmap (x :) (match wc (wc:ps) xs)
+longerWildcardMatch [] [] = Just []
+longerWildcardMatch _ [] = Nothing
+longerWildcardMatch [] _ Nothing
 
 
-=======
->>>>>>> cf816d78c3bf1dfc2a1652484b085c422a037e9b
+
 -- Test cases --------------------
 
 testPattern =  "a=*;"
@@ -182,21 +158,19 @@ substituteCheck = substituteTest == testString
 matchTest = match '*' testPattern testString
 matchCheck = matchTest == Just testSubstitutions
 
---------------------------------------------------------
+
+
+-------------------------------------------------------
 -- Applying patterns
 --------------------------------------------------------
 
 -- Applying a single pattern
 transformationApply :: Eq a => a -> ([a] -> [a]) -> [a] -> ([a], [a]) -> Maybe [a]
---transformationApply _ _ _ _ = Nothing
-{- TO BE WRITTEN -}
 transformationApply a f1 b (x, y) = mmap (substitute a y) (mmap f1 (match a x b))
 
 
 -- Applying a list of patterns until one succeeds
 transformationsApply :: Eq a => a -> ([a] -> [a]) -> [([a], [a])] -> [a] -> Maybe [a]
---transformationsApply _ _ _ _ = Nothing
-{- TO BE WRITTEN -}
 transformationsApply a f2 b list = foldl1 orElse (map (transformationApply a f2 list) b)
 
 
