ba2f660a8297e2dd1101790a90810254a996e3ff
jonathanloganmoran <jmoran23@ucmerced.edu>
2019-05-05 17:52:10 +0200

    Refactored optAlignments without using memoization

--- assignment2/StringAlignment.hs
+++ assignment2/StringAlignment.hs
@@ -1,27 +1,73 @@
 module StringAlignment where
 
+{- Smith-Waterman scoring penalties -}
 scoreMatch = 0
 scoreMismatch = -1
 scoreSpace = -1
 
+{- Needleman-Wunsch scoring penalties
+scoreMatch = 1
+scoreMismatch = -1
+scoreSpace = -2
+-}
+
 string1 = "writers"
 string2 = "vintner"
 
+{- Appends the prefix (first char) in a list of tuples -}
 attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])]
 attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
+
+{- Appends the second char in a list of tuples for left-to-right ordering -}
+attachTails :: a -> a -> [([a],[a])] -> [([a],[a])]
+attachTails h1 h2 aList = [(xs++[h1], ys++[h2]) | (xs,ys) <- aList]
 
 maximaBy :: Ord b => (a -> b) -> [a] -> [a]
 maximaBy _ [] = []
 maximaBy valueFcn xs = [ value | value <- xs, valueFcn value == maximum (map valueFcn xs)]
 
+{- .. where the local optimal alignment between xs and ys is determined by the resulting column score -}
 score :: Char -> Char -> Int
 score  x '-' = scoreSpace
 score '-' y  = scoreSpace
-score x y = if x == y then scoreMatch
-            else scoreMismatch
+score x y
+    | x == y = scoreMatch
+    | otherwise = scoreMismatch
 
+{- Caclulate similarity score for two non-empty strings .. -}
+-- WITHOUT memoization, to be optimized in future implementation
 similarityScore :: String -> String -> Int
 similarityScore [] [] = 0
 similarityScore [] ys = scoreSpace * length ys
 similarityScore xs [] = scoreSpace * length xs
-similarityScore (x:xs) (y:ys) = maximum [similarityScore xs ys + score x y , similarityScore xs (y:ys) + score x '-', similarityScore (x:xs) ys + score '-' y]
+-- where first element is the similarity score, second is the optimal alignment pair
+similarityScore (x:xs) (y:ys) = maximum [ similarityScore xs ys + score x y, 
+                                          similarityScore xs (y:ys) + score x '-', 
+                                          similarityScore (x:xs) ys + score '-' y ]
+
+{- Element type for storing intermediate table calculations -}
+type AlignmentType = (String,String)
+optAlignments :: String -> String -> [AlignmentType]
+
+
+{- Return list of all optimal allignments between string1 and string2 -}
+-- WITHOUT memoization, to be optimized in future implementation
+optAlignments [] [] = [([], [])]
+-- traverse string1 for matching char x then substring xs -}
+optAlignments (x:xs) [] = attachHeads x '-' (optAlignments xs [])
+-- traverse string2 for matching char y then substring ys -}
+optAlignments [] (y:ys) = attachHeads '-' y (optAlignments [] ys)
+optAlignments (x:xs) (y:ys) = maximaBy (uncurry similarityScore) pair
+	where pair = concat [ attachHeads x y (optAlignments xs ys),       {- Alignment #1 -}
+	                      attachHeads x '-' (optAlignments xs (y:ys)), {- Alignment #2 -}
+                          attachHeads '-' y (optAlignments (x:xs) ys)  {- Alignment #3 -} ]
+
+
+
+{- Print all optimal alignments between string1 and string2 -}
+outputOptAlignments :: String -> String -> IO ()
+outputOptAlignments string1 string2 = do
+	putStrLn . "Optimal alignments: \n"
+	{- print list here -}
+
+
