4de9a5a1fbec14e3ebec970dd23bfbb47b6d68d6
Abdeljalil Moussa <jalil.msa@gmail.com>
2019-05-17 13:15:43 +0200

    step1&2

--- assignment3/clean_parser_assignment/Parser.hs
+++ assignment3/clean_parser_assignment/Parser.hs
@@ -4,50 +4,57 @@ module Parser(module CoreParser, T, digit, digitVal, chars, letter, err,
 import Prelude hiding (return, fail)
 import Data.Char
 import CoreParser
-infixl 7 -#, #- 
+infixl 7 -#, #-
 
 type T a = Parser a
 
 err :: String -> Parser a
 err message cs = error (message++" near "++cs++"\n")
 
-iter :: Parser a -> Parser [a]  
-iter m = m # iter m >-> cons ! return [] 
+iter :: Parser a -> Parser [a]
+iter m = m # iter m >-> cons ! return []
 
 cons(a, b) = a:b
 
+--HERE
 (-#) :: Parser a -> Parser b -> Parser b
-m -# n = error "-# not implemented"
+m -# n = m # n >-> snd
 
+--HERE
 (#-) :: Parser a -> Parser b -> Parser a
-m #- n = error "#- not implemented"
+m #- n = m # n >-> fst
 
+--here
 spaces :: Parser String
-spaces =  error "spaces not implemented"
+spaces = iter (char ? isSpace)
 
 token :: Parser a -> Parser a
 token m = m #- spaces
 
+--here
 letter :: Parser Char
-letter =  error "letter not implemented"
+letter = char ? isAlpha
 
 word :: Parser String
 word = token (letter # iter letter >-> cons)
 
+--here
 chars :: Int -> Parser String
-chars n =  error "chars not implemented"
+chars 0 = return []
+chars n = char # chars (n-1) >-> cons
 
 accept :: String -> Parser String
 accept w = (token (chars (length w))) ? (==w)
 
+--here
 require :: String -> Parser String
-require w  = error "require not implemented"
+require wrd = (token (chars (length wrd))) ? (==wrd) ! err ("expecting " ++ wrd)
 
 lit :: Char -> Parser Char
 lit c = token char ? (==c)
 
-digit :: Parser Char 
-digit = char ? isDigit 
+digit :: Parser Char
+digit = char ? isDigit
 
 digitVal :: Parser Integer
 digitVal = digit >-> digitToInt >-> fromIntegral
@@ -57,4 +64,3 @@ number' n = digitVal #> (\ d -> number' (10*n+d))
           ! return n
 number :: Parser Integer
 number = token (digitVal #> number')
-
