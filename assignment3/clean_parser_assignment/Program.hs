b1a706365c37ccdca559578f8e4fc624340ea26c
jonathanloganmoran <jmoran23@ucmerced.edu>
2019-05-17 14:21:10 +0200

    Finishing Task 5:  module

--- assignment3/clean_parser_assignment/Program.hs
+++ assignment3/clean_parser_assignment/Program.hs
@@ -3,9 +3,19 @@ import Parser hiding (T)
 import qualified Statement
 import qualified Dictionary
 import Prelude hiding (return, fail)
-newtype T = Program () -- to be defined
-instance Parse T where
-  parse = error "Program.parse not implemented"
-  toString = error "Program.toString not implemented"
 
-exec = error "Program.exec not implemented"
+{- Task 5. Represent the program as a `Statement` list -}
+newtype T = Program [Statement.T] deriving Show
+
+{- Task 5. Define the parse function in this module -}
+instance Parse T where
+  parse = (iter Statement.parse) >-> Program
+  	--error "Program.parse not implemented"
+  toString (Program a) = foldr1 (++) (map Statement.toString a) 
+  	--error "Program.toString not implemented"
+
+{- Task 5. Use the `exec` function to execute a program -}
+exec :: T -> [Integer] -> [Integer]
+	--error "Program.exec not implemented"
+-- Hint 3d: Make a recursive call with a new dictionary
+exec (Program a) b = Statement.exec a Dictionary.empty b
